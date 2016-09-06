1a97754e9e6f675765b5d24c7697ee3494dfbc0c
Daniel <dela1000@gmail.com>
2016-09-05 19:25:37 -0700

    Update to ui-router

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 angular.module('home', [
-  'ngRoute'
+  'ui.router'
   ])
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
+.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+
+  // HOME STATES AND NESTED VIEWS ========================================
+  .state('home', {
+      url: '/home',
       templateUrl: 'app/about/about.html'
-    })
-    .when('/contact', {
-      templateUrl: 'app/contact/contact.html'
-    })
+  })
+
+  .state('contact', {
+    url: '/contact',
+    templateUrl: 'app/contact/contact.html'
+  })
 })
